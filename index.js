551d3955b7a92009ba316d667d8ddcab9bf5fce1
Duxlin <96833294+Duxlin@users.noreply.github.com>
2023-07-19 03:04:01 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('GURU -  BOT', {
+say('DUX -  BOT', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
